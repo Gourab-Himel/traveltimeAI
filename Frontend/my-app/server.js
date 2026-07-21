58b3675393f1dadb0c8536b04f547a963ee54f40
Gourab_Himel <2220577@iub.edu.bd>
2026-07-21 18:35:33 +0600

    Files are all checks

--- Frontend/my-app/server.js
+++ Frontend/my-app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const db = require('../Frontend/my-app/database/config');
+const db = require('./database/config');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
